68342c0d5b6ad0afd55ca41db014d360f4ea1080
Koval Maksym <srcmaxim@gmail.com>
2020-05-11 20:46:55 +0300

    #4 Create rest-blog service

---
+++ blog/rest-blog/src/test/java/com/github/srcmaxim/restblog/NativeBlogResourceIT.java
@@ -0,0 +1,9 @@
+package com.github.srcmaxim.restblog;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeBlogResourceIT extends BlogResourceTest {
+
+    // Execute the same tests but in native mode.
+}
